91ab53d6b8c290a66fcaa93fb40bb6fc57ebda22
vincenzo261003 <marinaro.manuel03@gmail.com>
2023-03-06 17:26:22 +0100

    'test.sql'

--- Week2/Giorno1/SQL/test.sql
+++ Week2/Giorno1/SQL/test.sql
@@ -1,19 +1,25 @@
--- DDL -> definisce la struttura
--- CREATE DATABASE lez00_rubrica;
--- DROP DATABASE lez00_rubrica;
+-- DDL (Data Definition Language) -> definisce la struttura
+DROP DATABASE IF EXISTS lez00_rubrica;
+CREATE DATABASE lez00_rubrica;
 USE lez00_rubrica;
 
 CREATE TABLE Persona(
-	nome VARCHAR(250),
-    cognome VARCHAR(250),
-    cf VARCHAR(16)
+	nome VARCHAR(250) NOT NULL,
+    cognome VARCHAR(250) NOT NULL,
+    telefono VARCHAR(250) DEFAULT "N.D.",
+    cf VARCHAR(16) UNIQUE
 );
 
--- DML -> manipola i dati nel db
+-- DML (Data Manipulation Language) -> manipola i dati nel db
 INSERT INTO Persona (nome, cognome, cf) 
-VALUES ("Manuel", "Marinaro", "MRNVCN03R26D912X"),
+VALUES ("Manuel", "Marinaro", "efjoiew"),
 ("Prova", "Uno", "Due");
 
 
--- QL -> interroga il DB
+-- QL (Query Language) -> interroga il DB
 SELECT * FROM Persona;
+
+ALTER TABLE Persona ADD COLUMN email VARCHAR(250) DEFAULT "N.D.";
+ALTER TABLE Persona DROP COLUMN email;
+ALTER TABLE Persona MODIFY COLUMN telefono INTEGER;
+
